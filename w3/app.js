5e085b8935b8e1cc994a71d513f75fb9207ccb63
Preston Norton <prestonanorton@gmail.com>
2026-01-29 17:26:38 -0800

    Step 2 complete

---
+++ w3/app.js
@@ -0,0 +1,5 @@
+import * as orderForm from "./order-handler.js";
+
+console.log('Hello from app.js! Your JavaScript is connected and running!');
+
+
---
+++ w3/order-handler.js
@@ -0,0 +1,26 @@
+
+const orderForm = document.getElementById('order-form'); // Query Selector functions
+const qtyInput = orderForm.querySelector('#qty');
+const giftWrapCheckbox = orderForm.querySelector('#gift-wrap');
+const sizeRadioButtons = orderForm.querySelectorAll('input[name="size"]');
+
+const getSelectedRadioValue = function(radioButtons) { // creates a helper function. Also writes a function to figure out which size radio button is checked
+    for (const radio of radioButtons) {
+        if (radio.checked) {
+        console.log(`${radio.value} is ${radio.checked}`);
+        return radio.value;
+        }
+    }
+};
+
+export const getOrderInputs = function() { // creates and exports a function
+    console.log('getOrderInputs is running...');
+    const orderFormSelections = {
+        qtyInputSelection: parseInt(qtyInput.value),
+        giftWrapCheckboxSelection: giftWrapCheckbox.checked,
+        sizeRadioButtonsSelection: getSelectedRadioValue(sizeRadioButtons)
+    };
+
+    console.log('getOrderInputs is returning...');
+    return orderFormSelections;
+};
