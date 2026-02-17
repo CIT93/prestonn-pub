f6b7f2aa71dafe91a319c19bd435ae16d6d51371
Preston Norton <prestonanorton@gmail.com>
2026-02-17 13:48:07 -0800

    Step 1 complete

---
+++ w6a/app.js
@@ -0,0 +1,34 @@
+import * as orderHandler from './order-handler.js';
+import * as priceCalculator from './price-calculator.js';
+import * as resultsDisplay from './results-display.js'; 
+
+const orderForm = document.getElementById('order-form');
+const summaryDiv = document.getElementById('order-summary');
+
+const orders = []; // Empty array to store all orders
+const handleOrderSubmit = function (event) {
+    event.preventDefault();
+
+    const orderData = orderHandler.getFormInputs(); // form data from the handler
+    console.log(orderData);
+
+    const calculatedPrice = priceCalculator.calculateTotal(orderData); // Calculate the price
+
+    
+    const newOrder = { // Marging operator uses this
+        ...orderData,
+        ...calculatedPrice,
+        timestamp: new Date().toISOString()
+    };
+
+    orders.push(newOrder);
+    console.log(orders);
+    resultsDisplay.displayOrder(newOrder);
+
+};
+
+const init = function () {
+    orderForm.addEventListener('submit', handleOrderSubmit);
+};
+
+document.addEventListener('DOMContentLoaded', init);
